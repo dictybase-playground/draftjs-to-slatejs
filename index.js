9a3259dea7be0ee6b38ea3d17dceb3c90895014d
wildlifehexagon <crossthebreeze@gmail.com>
2020-03-04 15:50:38 -0600

    refactor: use one convert function, add necessary json fields for patch request

--- index.js
+++ index.js
@@ -1,19 +1,16 @@
 require("dotenv").config()
 const fs = require("fs")
 const { downloadJSONs } = require("./src/download")
-const { draftjsToHTML } = require("./src/draft-to-html")
-const { htmlToSlate } = require("./src/html-to-slate")
 const { verifySlateData } = require("./src/verify")
+const { convertToSlate } = require("./src/convert")
 
 const fullConversion = async () => {
   try {
     fs.mkdirSync("draftjs", { recursive: true })
-    fs.mkdirSync("html", { recursive: true })
     fs.mkdirSync("slate", { recursive: true })
 
     await downloadJSONs("draftjs")
-    await draftjsToHTML("draftjs", "html")
-    await htmlToSlate("html", "slate")
+    await convertToSlate("draftjs", "slate")
     await verifySlateData("slate")
   } catch (error) {
     console.log(error)
