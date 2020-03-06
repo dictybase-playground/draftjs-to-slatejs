fef4f7dbe2c3a5718aa73e45c4c7df1380386ec9
wildlifehexagon <crossthebreeze@gmail.com>
2020-03-06 14:47:09 -0600

    refactor: clean up GetDraftjsContent function

---
+++ internal/minio/minio.go
@@ -0,0 +1,35 @@
+package minio
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/minio/minio-go/v6"
+	"github.com/urfave/cli"
+)
+
+func NewMinioClient(c *cli.Context) (*minio.Client, error) {
+	return minio.New(
+		fmt.Sprintf("%s:%s", c.String("minio-host"), c.String("minio-port")),
+		c.String("minio-access-key"),
+		c.String("minio-secret-key"),
+		false,
+	)
+}
+
+func MakeBucket(c *cli.Context, minioClient *minio.Client) error {
+	bucket := c.String("minio-bucket")
+	location := c.String("minio-location")
+	err := minioClient.MakeBucket(bucket, location)
+	if err != nil {
+		exists, errBucketExists := minioClient.BucketExists(bucket)
+		if errBucketExists == nil && exists {
+			log.Printf("%s already exists \n", bucket)
+		} else {
+			return cli.NewExitError(fmt.Sprintf("error creating bucket %s", err), 2)
+		}
+	} else {
+		log.Printf("Successfully created bucket %s\n", bucket)
+	}
+	return err
+}
