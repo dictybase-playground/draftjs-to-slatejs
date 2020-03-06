ce68c51557c74195246a09cb6e7fb655126ecdb3
wildlifehexagon <crossthebreeze@gmail.com>
2020-03-06 12:13:08 -0600

    feat: add command for running nodejs script

---
+++ internal/app/convert/convert.go
@@ -0,0 +1,22 @@
+package convert
+
+import (
+	"os"
+	"os/exec"
+
+	"github.com/urfave/cli"
+)
+
+func ConvertContent(c *cli.Context) error {
+	host := c.String("minio-host")
+	port := c.String("minio-port")
+	ak := c.String("minio-access-key")
+	sk := c.String("minio-secret-key")
+	b := c.String("minio-bucket")
+	id := c.String("user-id")
+	command := exec.Command("node-cli", "convert", "--minioHost", host, "--minioPort", port, "--accessKey", ak, "--secretKey", sk, "--bucket", b, "--userId", id)
+	command.Stdout = os.Stdout
+	command.Stderr = os.Stderr
+	command.Run()
+	return nil
+}
